5120be80b7d955bcda5fb71d9f83172b99460deb
micahj <micah.webb@talkingtech.com>
2019-09-20 15:19:40 +1200

    try sys std out to see if we can get something to the console

--- main.py
+++ main.py
@@ -1,6 +1,15 @@
+import sys
+
+sys.stdout.write('stdout ness\n')
+
+print(sys.platform)
+
+print('in yokohama module')
+
 
 def main():
     print("hi from yokohama")
 
-if __name__=='__main__':
+
+if __name__ == '__main__':
     main()
