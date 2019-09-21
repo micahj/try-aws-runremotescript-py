0ba580e122af2f92195133c86128aea7796bf919
micahj <micah.webb@talkingtech.com>
2019-09-21 12:07:20 +1200

    no output from py trying sh

--- main_b.py
+++ main_b.py
@@ -1,2 +1,2 @@
-print("Hello from main_b.py hopefully being execute inside an instance")
-raise Exception("Just need to see if this guy can capture output")
+print('1010101')
+# raise Exception("Just need to see if this guy can capture output")
